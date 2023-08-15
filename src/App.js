2935fe6c5d90807f62d5aef36b74054edf36fd5f
Rsam <arsam.hb@gmail.com>
2023-08-15 17:07:13 +0330

    my first component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  
+  const handleRandomName = () => {
+    const names = ["arsam","navid","hesam"];
+    const intNum = Math.floor(Math.random()*3)
+    return names[intNum]
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This is {handleRandomName()}'s code.</p>
       </header>
     </div>
   );
