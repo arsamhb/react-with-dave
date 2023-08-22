f9e5e07c0522b24711151c4034b641555e4a5d01
Rsam <arsam.hb@gmail.com>
2023-08-22 16:07:12 +0330

    used useEFfect

--- src/App.js
+++ src/App.js
@@ -3,30 +3,29 @@ import SearchItem from "./components/SearchItem";
 import Content from "./components/Content";
 import AddForm from "./components/AddForm";
 import Footer from "./components/Footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("RsList"))
+    JSON.parse(localStorage.getItem("RsList")) || []
   );
   const [newItem, setNewItem] = useState("");
   const [searchWord, setSearchWord] = useState("");
 
-  const setAndSaveItems = (newListOfItems) => {
-    setItems(newListOfItems);
-    localStorage.setItem("RsList", JSON.stringify(newListOfItems));
-  };
+  useEffect(() => {
+    localStorage.setItem("RsList", JSON.stringify(items));
+  }, [items]);
 
   const handleCheck = (id) => {
     const tasks = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSaveItems(tasks);
+    setItems(tasks);
   };
 
   const handleDelete = (id) => {
     const tasks = items.filter((item) => item.id !== id);
-    setAndSaveItems(tasks);
+    setItems(tasks);
   };
 
   const handleSubmit = (e) => {
@@ -40,7 +39,7 @@ function App() {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const checked = false;
     const tasks = [...items, { id: id, checked: checked, item: newItem }];
-    setAndSaveItems(tasks);
+    setItems(tasks);
   };
 
   return (
